efc37fa9aba9ab00c52ecf8cd62503b45022a4bb
Maksim Kostromin <daggerok@gmail.com>
2019-07-11 01:39:00 +0300

    bla-bla tests...

--- idea-sbt/src/test/scala/com/github/daggerok/specs/FutureSpec.scala
+++ idea-sbt/src/test/scala/com/github/daggerok/specs/FutureSpec.scala
@@ -6,6 +6,7 @@ import scala.concurrent._
 import ExecutionContext.Implicits.global
 import scala.language.postfixOps
 import scala.concurrent.duration._
+import scala.util.{Failure, Random}
 
 /**
   * http://www.scalatest.org/user_guide/selecting_a_style
@@ -33,4 +34,29 @@ class FutureSpec extends FlatSpec {
     val list = Await.result(forFuture, 1 seconds)
     assert(list == List("Hola, Maksimko!"))
   }
+
+  //"Fuck that shit! Future" should "have be able to retry with recover and foldLeft" in {
+  //  object CanFail {
+  //    private val random = new Random
+  //    def maybeHello(name: String): Future[String] = Future[String] {
+  //      if (random.nextBoolean()) s"Hello, $name!"
+  //      else throw new RuntimeException("bye...")
+  //    }
+  //    def retry[T](times: Int = 1)(block: => Future[T]): Future[T] = {
+  //      val failure: Future[Nothing] = Future.failed(new RuntimeException(
+  //        s"sorry, I tried $times, but not got lucky..."))
+  //      if (times < 1) failure
+  //      else {
+  //        val retries = (0 to times).map(_ => () => block)
+  //        val result = retries
+  //          //.foldLeft(failure)((failed, blck) => () => { failed.recoverWith { blck() } })
+  //          .foldRight(failure)((blck, failed) => () => {
+  //                                                  blck() fallbackTo { failed }
+  //                                                })
+  //        result()
+  //      }
+  //    }
+  //  }
+  //  import CanFail._
+  //}
 }
